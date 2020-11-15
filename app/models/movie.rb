f60b46bc25a1135f120c0cad82afff946317aa91
Arthur PV <apetersonveatch@gmail.com>
2020-11-15 16:28:44 -0600

    adds genre table and MovieGenre join table

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -3,4 +3,6 @@ class Movie < ApplicationRecord
   validates :plot, presence: true, length: { minimum: 10 }
 
   has_many :actors
+  has_many :movie_genres
+  has_many :genres, through: :movie_genres
 end
